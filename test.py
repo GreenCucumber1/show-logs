432d9fcc8e0bd38ed1ce65444ea0e620372f5d4d
Teplov Pavel <90750367+GreenCucumber1@users.noreply.github.com>
2024-04-05 13:11:23 +0300

    Update test.py

--- test.py
+++ test.py
@@ -4,26 +4,25 @@ import plotly
 import plotly.express as px
 
 
-def main():
-    df6 = pd.read_excel('df5.xlsx')
-    df6 = df6[df6['start'] >= '2024-03-18']
-    df7 = pd.read_excel('df7.xlsx')
-    df7 = df7[df7['start'] >= '2024-03-25']
-    color_map = {'FinishedSuccess': 'blue', 'FinishedFail': 'red', 'Skipped': 'yellow','Error': 'purple','Reset': 'orange'}
+df6 = pd.read_excel('df5.xlsx')
+df6 = df6[df6['start'] >= '2024-03-18']
+df7 = pd.read_excel('df7.xlsx')
+df7 = df7[df7['start'] >= '2024-03-25']
+color_map = {'FinishedSuccess': 'blue', 'FinishedFail': 'red', 'Skipped': 'yellow','Error': 'purple','Reset': 'orange'}
     
-    def prod():
+def prod():
         st.subheader("за неделю prod")
         fig = px.timeline(df6, x_start="start", x_end="end", y="AppName",color = 'Status',color_discrete_map=color_map)
         fig.update_layout(width=800, height=1600)
         st.plotly_chart(fig)
         
-    def dev():
+def dev():
         st.subheader("за неделю dev")
         fig = px.timeline(df7, x_start="start", x_end="end", y="AppName",color = 'Status',color_discrete_map=color_map)
         fig.update_layout(width=800, height=1600)
         st.plotly_chart(fig)
         
-    def main():
+def main():
         st.sidebar.title("Навигация")
         pages = {
             "Prod": prod,
