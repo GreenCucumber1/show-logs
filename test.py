0b434d76948764d76f20128209a5ae19ded50f60
Teplov Pavel <90750367+GreenCucumber1@users.noreply.github.com>
2024-04-05 12:54:19 +0300

    Update test.py
    add dev plot

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ import plotly.express as px
 
 
 def main():
-      # график timeline за неделю
+    # график timeline за неделю prod
     st.subheader("за неделю")
     df6 = pd.read_excel('df5.xlsx')
     df6 = df6[df6['start'] >= '2024-03-18']
@@ -13,5 +13,16 @@ def main():
     fig = px.timeline(df6, x_start="start", x_end="end", y="AppName",color = 'Status',color_discrete_map=color_map)
     fig.update_layout(width=800, height=1600)
     st.plotly_chart(fig)
+      
+    # график timeline за неделю dev
+    st.subheader("за неделю")
+    df7 = pd.read_excel('df7.xlsx')
+    df7 = df7[df7['start'] >= '2024-03-25']
+    color_map = {'FinishedSuccess': 'blue', 'FinishedFail': 'red', 'Skipped': 'yellow','Error': 'purple','Reset': 'orange'}
+    fig = px.timeline(df7, x_start="start", x_end="end", y="AppName",color = 'Status',color_discrete_map=color_map)
+    fig.update_layout(width=800, height=1600)
+    st.plotly_chart(fig)
+
+
 if __name__ == "__main__":
     main()
