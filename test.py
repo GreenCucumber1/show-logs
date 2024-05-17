210af1b5fd265b1bd24906ca82649c5f773556d7
Teplov Pavel <90750367+GreenCucumber1@users.noreply.github.com>
2024-05-17 17:14:24 +0300

    Update test.py

--- test.py
+++ test.py
@@ -2,36 +2,45 @@ import streamlit as st
 import pandas as pd
 import plotly
 import plotly.express as px
+import datetime
 
-
-df6 = pd.read_excel('df5.xlsx')
-df6 = df6[df6['start'] >= '2024-03-18']
-df7 = pd.read_excel('df7.xlsx')
-df7 = df7[df7['start'] >= '2024-03-25']
 color_map = {'FinishedSuccess': 'blue', 'FinishedFail': 'red', 'Skipped': 'yellow','Error': 'purple','Reset': 'orange'}
     
-def prod():
-        st.subheader("за неделю prod")
-        fig = px.timeline(df6, x_start="start", x_end="end", y="AppName",color = 'Status',color_discrete_map=color_map)
-        fig.update_layout(width=800, height=1600)
-        st.plotly_chart(fig)
+def prod(df, start_date, end_date):
+    st.subheader("за неделю prod")
+    df = df[(df['start'] >= start_date) & (df['start'] <= end_date)]
+    fig = px.timeline(df, x_start="start", x_end="end", y="AppName",color = 'Status',color_discrete_map=color_map)
+    fig.update_layout(width=800, height=1600)
+    st.plotly_chart(fig)
         
-def dev():
-        st.subheader("за неделю dev")
-        fig = px.timeline(df7, x_start="start", x_end="end", y="AppName",color = 'Status',color_discrete_map=color_map)
-        fig.update_layout(width=800, height=1600)
-        st.plotly_chart(fig)
+def dev(df, start_date, end_date):
+    st.subheader("за неделю dev")
+    df = df[(df['start'] >= start_date) & (df['start'] <= end_date)]
+    fig = px.timeline(df, x_start="start", x_end="end", y="AppName",color = 'Status',color_discrete_map=color_map)
+    fig.update_layout(width=800, height=1600)
+    st.plotly_chart(fig)
         
 def main():
-        st.sidebar.title("Навигация")
-        pages = {
-            "Prod": prod,
-            "Dev": dev
-        }
-        selection = st.sidebar.radio("Переключиться на:", list(pages.keys()))
-        page = pages[selection]
-        page()
+    st.sidebar.title("Навигация")
+    pages = {
+        "Prod": prod,
+        "Dev": dev
+    }
+    selection = st.sidebar.radio("Переключиться на:", list(pages.keys()))
+    page = pages[selection]
+    today = datetime.date.today()
+    start_date = today - datetime.timedelta(days=today.weekday())
+    start_date = st.sidebar.date_input('Выберите начальную дату',value=start_date)
+    end_date = st.sidebar.date_input('Выберите конечную дату',value=today)
 
+    # Преобразование в объекты datetime
+    start_date = datetime.datetime.combine(start_date, datetime.time())
+    end_date = datetime.datetime.combine(end_date, datetime.time())
+
+    df6 = pd.read_excel('logs_prod.xlsx')
+    df7 = pd.read_excel('logs_dev.xlsx')
+
+    page(df6 if selection == "Prod" else df7, start_date, end_date)
 
 if __name__ == "__main__":
     main()
